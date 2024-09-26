0d3f8fbe9940985d7b2de12f6ab1126dacd0d370
Sergey Slom <slomser@mail.ru>
2024-09-27 00:01:56 +0300

    working without structures

--- my_strcmp.cpp
+++ my_strcmp.cpp
@@ -18,84 +18,25 @@ int my_strcmp(const void *line1, const void *line2)
     const char *str1 = *(const char **)line1;
     const char *str2 = *(const char **)line2;
 
+    size_t current_symbol1 = 0, current_symbol2 = 0;
+
+    char temp1 = '0';
+    char temp2 = '0';
+
+    while(str1[current_symbol1] && str2[current_symbol2])
+    {
+        while(isdigit(str1[current_symbol1]) && ispunct(str1[current_symbol1]) && isspace(str1[current_symbol1])) current_symbol1++;
+        while(isdigit(str2[current_symbol2]) && ispunct(str2[current_symbol2]) && isspace(str2[current_symbol2])) current_symbol2++;
+
+        temp1 = str1[current_symbol1];
+        temp2 = str2[current_symbol2];
+
+        temp1 = (char)tolower(temp1);
+        temp2 = (char)tolower(temp2);
+
+        if(temp1 < temp2) return -1;
+        else if(temp1 > temp2) return 1;
+    }
     
-
-    //return strcmp(str1, str2);
-
-    //.-, !?*;()':
-
-    char *strtemp1 = (char*)calloc(strlen(str1) + 1, sizeof(char));
-    char *strtemp2 = (char*)calloc(strlen(str2) + 1, sizeof(char));
-
-    // for(size_t current_elem = 0; current_elem < strlen(str1); current_elem++) strtemp1[current_elem] = tolower((int) str1[current_elem]);
-    // for(size_t current_elem = 0; current_elem < strlen(str2); current_elem++) strtemp2[current_elem] = tolower((int) str2[current_elem]);
-
-   // int tempelem1 = 0, tempelem2 = 0;
-   // int isletter1 = -5, isletter2 = -5;
-    size_t current_temp1 = 0, current_temp2 = 0;
-
-    size_t minlen = 0;
-
-    if(strlen(str1) < strlen(str2)) minlen = strlen(str2);
-    else minlen = strlen(str1);
-
-    size_t current_elem = 0;
-
-    //strcat(strtemp1, "\0");
-
-    while(str1[current_elem] != '\0' && str2[current_elem] != '\0')
-    {
-        if( str1[current_elem] != '.' && 
-            str1[current_elem] != '-' &&
-            str1[current_elem] != ',' &&
-            str1[current_elem] != ' ' &&
-            str1[current_elem] != '!' &&
-            str1[current_elem] != '?' &&
-            str1[current_elem] != '*' &&
-            str1[current_elem] != ';' &&
-            str1[current_elem] != '(' &&
-            str1[current_elem] != ')' &&
-            str1[current_elem] != '"' &&
-            str1[current_elem] != ':')
-            {
-                strtemp1[current_temp1] = (char) tolower(str1[current_elem]);
-                current_temp1++;
-            }
-
-        if( str2[current_elem] != '.' && 
-            str2[current_elem] != '-' &&
-            str2[current_elem] != ',' &&
-            str2[current_elem] != ' ' &&
-            str2[current_elem] != '!' &&
-            str2[current_elem] != '?' &&
-            str2[current_elem] != '*' &&
-            str2[current_elem] != ';' &&
-            str2[current_elem] != '(' &&
-            str2[current_elem] != ')' &&
-            str2[current_elem] != '"' &&
-            str2[current_elem] != ':')
-            {
-                strtemp2[current_temp2] = (char) tolower(str2[current_elem]);
-                current_temp2++;
-            }
-
-            current_elem++;
-    }
-
-    minlen = 0;
-
-    if(strlen(strtemp1) < strlen(strtemp2)) minlen = current_temp2;
-    else minlen = current_temp1;
-
-    for(size_t temp_elem = 0; temp_elem < minlen; temp_elem++)
-    {
-        if(strtemp1[temp_elem] < strtemp2[temp_elem]) return -1;
-        else if(strtemp1[temp_elem] > strtemp2[temp_elem]) return 1;
-
-        if(strtemp1[temp_elem] == '\0' && strtemp2[temp_elem] == '\0') return 0;
-        else if(strtemp1[temp_elem] == '\0') return -1;
-        else if(strtemp2[temp_elem] == '\0') return 1;
-    }
-
-    return 999;
+    return 0;
 }
